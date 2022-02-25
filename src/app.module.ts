4c31e83da5cefe8322addecf1e164b3db460fcc2
jarek <ja.jarek@gmail.com>
2022-02-25 23:14:06 +0100

    deploy test version

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { GetCardService } from './get-card/get-card.service';
-import { GetCardController } from './get-card/get-card.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, GetCardController],
-  providers: [AppService, GetCardService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
